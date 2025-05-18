e446348e084c2e17196340f79b62ae2bccbf2e44
Geek Neuron <geekneuron@gmail.com>
2025-05-18 22:38:37 +0330

    Delete scripts/dns_revert.ps1

--- scripts/dns_revert.ps1
+++
@@ -1,9 +0,0 @@
-# ZenDNSChanger - بازگردانی DNS به حالت قبل
-$interface = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
-$prevDns = Get-Content "$env:ProgramData\zendns_prev.txt"
-if ($prevDns) {
-  Set-DnsClientServerAddress -InterfaceIndex $interface.ifIndex -ServerAddresses $prevDns
-  Write-Host "[✓] DNS reverted to: $prevDns"
-} else {
-  Write-Host "[!] No backup DNS found"
-}
