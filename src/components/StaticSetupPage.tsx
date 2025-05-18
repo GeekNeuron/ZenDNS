2631952cc0e9e6ffc2bd913cdb6d534836173f22
Geek Neuron <geekneuron@gmail.com>
2025-05-18 18:22:55 +0330

    Create StaticSetupPage.tsx

---
+++ src/components/StaticSetupPage.tsx
@@ -0,0 +1,9 @@
+export default function StaticSetupPage() {
+  return (
+    <div className="p-4 max-w-xl mx-auto">
+      <h1 className="text-xl font-bold">ZenDNS Manual Setup</h1>
+      <p>Use <code>https://yourdomain.com/dns-query</code> in your system settings.</p>
+      <p><a href="/setup.html" className="underline text-blue-600">Open full setup page</a></p>
+    </div>
+  );
+}
