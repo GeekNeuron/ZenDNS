915afe31cd28a5ba02d0958a6f863d65d30a4acc
Geek Neuron <geekneuron@gmail.com>
2025-05-19 12:30:59 +0330

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Logs from './screens/Logs';
+import Stats from './screens/Stats';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Logs" component={Logs} />
+        <Stack.Screen name="Stats" component={Stats} />
       </Stack.Navigator>
     </NavigationContainer>
   );
